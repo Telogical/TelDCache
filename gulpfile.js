5f2dfa585960a08e063de1528afdbca7f2cadb52
Joe Hare <joseph.hare@gmail.com>
2015-03-02 09:48:15 -0600

    Refactoring gulp stuff (#1)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var build = require('./gulp/build.js');
-var lint = require('./gulp/lint.js');
-var test = require('./gulp/test.js');
-var watch = require('./gulp/watch.js');
+require('./gulp/build.js');
+require('./gulp/lint.js');
+require('./gulp/test.js');
+require('./gulp/watch.js');
