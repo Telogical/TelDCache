9654335a34d52f61bdd4dba9efe2e2bc0f867e9c
Joe Hare <joseph.hare@gmail.com>
2015-03-17 14:33:04 -0500

    Implement helpful error message (#1)

--- gulp/test.js
+++ gulp/test.js
@@ -4,11 +4,16 @@ var options = require('./options');
 var gulp = require('gulp');
 var mocha = require('gulp-mocha'); 
 
+function handleError(error) {
+  throw new Error("Unit tests has failed! pls2befixingkthx");
+}
+
 function runUnitTests() {
   gulp.src(options.testFiles)
     .pipe(mocha({
       'reporter': 'nyan'
-    }));
+    }))
+    .on('error', handleError);
 }
 
 gulp.task('test', runUnitTests);
